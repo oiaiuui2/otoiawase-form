89dda436ad3e55ac4da71a70a157cb7c7f43ce5c
落合来弥 <badkuku567@gmail.com>
2022-08-24 22:36:16 +0900

    create form

---
+++ complete.php
@@ -0,0 +1,45 @@
+<?php 
+$name = $_POST["name"];
+$email = $_POST["email"];
+$form = $_POST["form"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>お問い合わせ完了</title>
+        <link href="styl.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>お問い合わせ完了</h1>
+        <p>お問い合わせが完了いたしました。</p>
+        <p>内容をご確認ください。</p>
+        
+        <div class="joho">
+            <div>
+            <span>名前:</span>
+            <?php 
+            echo $name;
+            ?>
+            </div>
+
+            <div>
+            <span>メールアドレス:</span>
+            <?php
+            echo $email;
+            ?>
+            </div>
+
+            <div>
+            <span>お問い合わせ内容:</span>
+            <?php
+            echo $form;
+            ?>
+            </div>
+        </div>
+        
+    </body>
+</html>
